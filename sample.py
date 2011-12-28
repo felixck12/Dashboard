8f0bd03a97346de7a42da72201864fe7c5576f87
Felix <felix@ck12.org>
2011-12-28 14:54:00 -0800

    testing api

---
+++ sample.py
@@ -0,0 +1,19 @@
+import urllib2
+
+
+def run():
+	return {
+	"points":{
+		"point":[
+			{"city":{"city_name":"london","country_code":"GB"}},
+			{"city":{"city_name":"San Francisco","region_code":"CA","country_code":"US"}},
+			{"latitude" : "51.526263", "longitude" : "-0.092429"},
+			{"latitude" : "-33.94336", "longitude" : "18.896484"},
+			{"host" : "geckoboard.com"},
+			{"ip" : "178.125.193.227"}
+		]
+	}
+}
+
+
+run()
